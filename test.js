daa33aaac0394ebe8374779554aef52aa5866b5c
Sparsh7082 <sparshgupta2109@gmail.com>
2024-11-19 23:22:21 +0530

    js2.3.1

---
+++ test.js
@@ -0,0 +1,26 @@
+function connecttoDiceServer(){
+    console.log("connectting ......")
+  setTimeout(() => {
+        console.log("connected to dice server")
+        
+    }, 3000);
+}
+
+function exploreCourse(){
+    console.log("exploring-------")
+    setTimeout(() => {
+        console.log("courses explored")
+        
+    }, 5000);
+}
+
+function selectCourse(){
+    console.log("course selection done")
+}
+function onboard(){
+    console.log("class le rhi hu")
+}
+connecttoDiceServer();
+exploreCourse();
+selectCourse();
+onboard();
